1475dd4201e847702fff5bb9529bbf07deeb36e6
Tim Feirg <kkcocogogo@gmail.com>
2014-03-23 00:30:09 +0800

    add pics, weighted average filter as described in p120

--- main.cpp
+++ main.cpp
@@ -36,26 +36,40 @@ int main(int argc, const char * argv[])
         {(float)1/9,(float)1/9,(float)1/9},
         {(float)1/9,(float)1/9,(float)1/9}
     };
+    // another text book weighted average blur filter, described in page 120
+    float weighted_avg_array[3][3] = {
+        {(float)1/16,(float)2/16,(float)1/16},
+        {(float)2/16,(float)4/16,(float)2/16},
+        {(float)1/16,(float)2/16,(float)1/16}
+    };
     // the laplacian filter is designed based on the coefficients I found on internet
     float laplacian_array[3][3] = {
         {-1,-1,-1},
         {-1,8,-1},
         {-1,-1,-1}
     };
+
     
     // creating filter as Mat objects based on the array just designed
     Mat box_filter = Mat( 3, 3, CV_32F, box_array ),
-    laplacian_filter = Mat( 3, 3, CV_32F, laplacian_array);
+    laplacian_filter = Mat( 3, 3, CV_32F, laplacian_array ),
+    weighted_avg_filter = Mat( 3, 3, CV_32F, weighted_avg_array);
     
+    // start filtering in spatial domain, for convenience sake I store result of each process in the same target_image object, apologies
     filter2D( source_image, target_image, -1, box_filter );
-    imshow("box filter", target_image);
+    imshow("box filter blur effect", target_image);
     
-    filter2D( source_image, target_image, -1, laplacian_filter);
+    filter2D( source_image, target_image, -1, weighted_avg_filter);
+    imshow("weighted average", target_image);
+    
+    filter2D( source_image, target_image, -1, laplacian_filter );
     imshow("Laplacian filter", target_image);
+    
     // high-boost filtering from what I know is original image + Laplacian filtered image
     float highboost_coefficient = 1;
     target_image += highboost_coefficient * source_image;
-    imshow("high-boost filter", target_image);
+    
+    imshow( "high-boost filter", target_image );
     waitKey();
     
     return 0;
